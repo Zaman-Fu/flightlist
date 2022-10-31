06b92b61b8872700a4b204ce9c005061504ea68b
Philip Zamanis <philip.zamanis@solera.com>
2022-10-31 16:10:11 +0100

    spring initialization state

---
+++ src/main/java/skyviewer/service/flightlist/FlightlistApplication.java
@@ -0,0 +1,13 @@
+package skyviewer.service.flightlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/skyviewer/service/flightlist/FlightlistApplicationTests.java
@@ -0,0 +1,13 @@
+package skyviewer.service.flightlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
